eabfd53aa34d1dc8272cb94075df093795d6d17b
Jorge Murta <jorge.murta@turbinekreuzberg.com>
2022-09-29 08:42:22 +0100

    fix codesniffer notices

--- src/TurbineKreuzberg/Shared/Sentry/SentryConstants.php
+++ src/TurbineKreuzberg/Shared/Sentry/SentryConstants.php
@@ -4,8 +4,20 @@ namespace TurbineKreuzberg\Shared\Sentry;
 
 interface SentryConstants
 {
+    /**
+     * @var string
+     */
     public const DSN = 'SENTRY:CONFIG:DSN';
+    /**
+     * @var string
+     */
     public const IGNORED_EXCEPTIONS = 'SENTRY:CONFIG:IGNORED_EXCEPTIONS';
+    /**
+     * @var string
+     */
     public const TRACE_SAMPLE_RATE = 'SENTRY:CONFIG:TRACE_SAMPLE_RATE';
+    /**
+     * @var string
+     */
     public const APPLICATION_VERSION = 'SENTRY:CONFIG:APPLICATION_VERSION';
 }
--- src/TurbineKreuzberg/Shared/Sentry/SentryVersion.php
+++ src/TurbineKreuzberg/Shared/Sentry/SentryVersion.php
@@ -4,6 +4,12 @@ namespace TurbineKreuzberg\Shared\Sentry;
 
 final class SentryVersion
 {
+    /**
+     * @var string
+     */
     public const SDK_IDENTIFIER = 'sentry.php.spryker';
+    /**
+     * @var string
+     */
     public const SDK_VERSION = '0.1.0';
 }
